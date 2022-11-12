45921add1190383934ce37222c99a3a40cb563fc
St. Elmo Wilken <stelmozors@gmail.com>
2022-11-12 13:41:27 +0100

    converted into a project

--- src/metabolism/glycolysis_gluconeogensis.jl
+++ src/metabolism/glycolysis_gluconeogensis.jl
@@ -3,6 +3,7 @@ module Glycolysis_Gluconeogenesis
 using ..ModuleTools
 using ..Utils
 
+using COBREXA
 using COBREXA.Types
 
 subsystem = "Glycolysis/Gluconeogenesis"
@@ -12,7 +13,7 @@ subsystem = "Glycolysis/Gluconeogenesis"
 rxns = (
     (rhea_id = 23536, name = "Phosphoglucomutase", isozymes = [[(1, "A0A1B1EB72"),], [(1, "A0A1B1EER5"),]], subsystem = subsystem,),
     (rhea_id = 11816, name = "Glucose-6-phosphate isomerase", isozymes = [[(1, "A0A1B1EFC4"),],], subsystem = subsystem,),
-    # (rhea_id = 11064, name = "Fructose-1,6-bisphosphatase", isozymes = [[(4, "A0A1B1ELE3"),], [(4, "A0A1B1ELE3"),],], subsystem = subsystem,),
+    (rhea_id = 11064, name = "Fructose-1,6-bisphosphatase", isozymes = [[(4, "A0A1B1ELE3"),], [(4, "A0A1B1ELE3"),],], subsystem = subsystem,),
 )
 
 end # module
