10f87c12f53ef3fca9df50348d0d850d43bc49c8
Fl-Sch <113851616+Fl-Sch@users.noreply.github.com>
2022-12-14 22:52:45 +0100

    some fixes

--- src/metabolism/his_metabolism.jl
+++ src/metabolism/his_metabolism.jl
@@ -12,11 +12,11 @@ subsystem = "Histidine Metabolism"
 
 rxns = (
     (rhea_id = 18473, name = "ATP phosphoribosyltransferase", isozymes = [[(1, "A0A1B1EBD2"),],], subsystem = subsystem,), #2.4.2.17
-    (rhea_id = 22828, name = "Histidine biosynthesis bifunctional protein HisIE", isozymes = [[(1, "A0A1B1EBE1"),],], subsystem = subsystem,), #3.6.1.31?
-    (rhea_id = 20049, name = "Histidine biosynthesis bifunctional protein HisIE", isozymes = [[(1, "A0A1B1EBE1"),],], subsystem = subsystem,), #3.5.4.19?
+    (rhea_id = 22828, name = "Histidine biosynthesis bifunctional protein HisIE", isozymes = [[(1, "A0A1B1EBE1"),],], subsystem = subsystem,), #3.6.1.31
+    (rhea_id = 20049, name = "Histidine biosynthesis bifunctional protein HisIE", isozymes = [[(1, "A0A1B1EBE1"),],], subsystem = subsystem,), #3.5.4.19
     (rhea_id = 15469, name = "1-(5-phosphoribosyl)-5-[(5-phosphoribosylamino)methylideneamino] imidazole-4-carboxamide isomerase", isozymes = [[(1, "A0A1B1EBE2"),],], subsystem = subsystem,), #5.3.1.16
-    (rhea_id = 14465, name = "Histidine biosynthesis bifunctional protein HisB", isozymes = [[(1, "A0A1B1EBE7"),],], subsystem = subsystem,), #4.2.1.19 ?
-    (rhea_id = 11040, name = "Histidine biosynthesis bifunctional protein HisB", isozymes = [[(1, "A0A1B1EBE7"),],], subsystem = subsystem,), #3.1.3.15 ?
+    (rhea_id = 14465, name = "Imidazoleglycerol-phosphate dehydratase", isozymes = [[(1, "A0A1B1EBE7"),],], subsystem = subsystem,), #4.2.1.19 part of Histidine biosynthesis bifunctional protein HisB
+    (rhea_id = 11040, name = "Histidinol-phosphatase", isozymes = [[(1, "A0A1B1EBE7"),],], subsystem = subsystem,), #3.1.3.15 part of Histidine biosynthesis bifunctional protein HisB
     (rhea_id = 20641, name = "Histidinol dehydrogenase", isozymes = [[(1, "A0A1B1EBD6"),],], subsystem = subsystem,), #1.1.1.23
     (rhea_id = 21232, name = "Histidine ammonia-lyase", isozymes = [[(1, "A0A1B1EHA2"),],], subsystem = subsystem,), #4.3.1.3
     (rhea_id = 13101, name = "Urocanate hydratase", isozymes = [[(1, "A0A1B1EH77"),],], subsystem = subsystem,), #4.2.1.49
