cb202fcb1a981175eabf874d72512d92f4e8da6d
St. Elmo Wilken <stelmozors@gmail.com>
2025-01-20 10:08:28 +0100

    update transporters

--- src/transporters.jl
+++ src/transporters.jl
@@ -1,122 +1,4 @@
 
-# "C02084" # Tetrathionate
-# "C00087" # sulfur
-# "C06423" # Octanoic acid
-# "C01571" # Decanoic acid
-# "C02679" # Dodecanoic acid
-# "C06424" # Tetradecanoic acid
-# "C00154" # Palmitoyl-CoA
-# "C00249" # Hexadecanoic acid
-# "C01530" # Octadecanoic acid
-# "C00641" # 1,2-Diacyl-sn-glycerol
-# "C00075" # UTP
-# "C00013" # diphosphate
-# "C00004" # NADH
-# "C00003" # NAD+
-# "C00005" # NADPH
-# "C00006" # NADP+
-# "C00672" # 2-Deoxy-D-ribose 1-phosphate
-# "C01801" # Deoxyribose
-# "C00020" # AMP
-# "C00002" # atp
-# "C00008" # adp
-# "C00275" # mannose 6 phosphate
-# "C00546" # methylglyoxal
-# "C05993" # acetyl adenylate
-# "C00988" # Phosphoglycolic acid
-# "C00263" # homoserine
-# "C00154" # Palmitoyl-coa
-# "C00620" # alpha ribose 1 phosphate
-# "C00024" # Acetyl-CoA
-# "C00119" # PRPP    
-# "C00026" # 2-Oxoglutarate
-# "C00027" # h2o2
-# "C00167" # UDP-glucuronate
-# "C00111" # glycerone-P
-# "C00266" # glycoaldehyde
-# "C00424" # L-lactaldehyde
-# "C01050" # UDP-N-acetylmuramate 
-# "C00498" # ADP-glucose (to starch)
-# "C00636" # Mannose 1 phosphate
-# "C00160" # glycolate
-# "C00099" # beta alanine
-# "C00168" # Hydroxypyruvate
-# "C06010" # (S)-2-Acetolactate
-# "C00877" # crotonyl-coa
-# "C05359" # electron
-# "C15602" # Quinone
-# "C15603" # Hydroquinone
-# "C00016" # FAD
-# "C01352" # FADH2
-# "C00004" # NADH
-# "C00003" # NAD+
-# "C00138" # Reduced ferredoxin
-# "C00139" # Oxidized ferredoxin
-# "C00163" # propanoate
-# "C00100" # propanoyl coa
-# "C00342" # thioredoxin
-# "C00343" # thioredoxin disulfide
-# "C00125" # ferricytochrome c
-# "C00126" # ferrocytochrome c
-# "C00924" # Ferrocytochrome 
-# "C00923" # Ferricytochrome 
-# "C00101" # Tetrahydrofolate 
-# "C00143" # 5,10-Methylenetetrahydrofolate
-# "C00234" # 10-Formyltetrahydrofolate
-# "C00332" # acetoacetyl-coa
-# "C01127" # 4-Hydroxy-2-oxoglutarate
-# "C05984" # 2-Hydroxybutanoic acid
-# "C00222" # 3-Oxopropanoate
-# "C00054" # Adenosine 3',5'-bisphosphate (PAP)
-# "C00229" # ACP
-# "C00010" # coa
-# "C00697" # n2
-# "C00244" # nitrate
-# "C00121" # ribose
-# "C00257" # D-Gluconic acid
-# "C00259" # L-arabinose
-# "C01721" # L-Fuculose
-# "C00310" # D-Xylulose 
-# "C00216" # D-Arabinose
-# "C00508" # L-ribulose
-# "C00312" # L-xylulose
-# "C00507" # L-Rhamnose
-# "C01019" # L-fucose
-# "C00095" # D-fructose
-# "C00124" # D-galactose
-# "C00243" # Lactose
-# "C05796" # Galactan
-# "C00492" # Raffinose
-# "C01613" # Stachyose
-# "C00208" # Maltose
-# "C00329" # Glucosamine
-# "C00140" # N-Acetyl-Glucosamine
-# "C01674" # Chitobiose
-# "C00116" # Glycerol
-# "C00267" # Alpha-glucose
-# "C00031" # D-Glucose
-# "C00022" # Pyruvate
-# "C00033" # Acetate
-# "C00058" # Formate
-# "C00186" # (S)-Lactate
-# "C00469" # Ethanol
-# "C00122" # Fumarate
-# "C00042" # Succinate
-# "C01904" # D-Arabitol
-# "C00256" # (R)-lactate
-# "C00288" # hco3
-# "C00064" # L-glutamine
-# "C00025" # glutamate
-# "C00283" # hydrogen sulfide
-# "C00097" # L-cysteine
-# "C00282" # h2
-# "C00011" # CO2
-# "C00001" # water
-# "C00080" # h
-# "C00037" # glycine
-# "C00065" # serine
-# "C00041" # L-alanine
-
 """
 $(TYPEDSIGNATURES)
 
@@ -198,20 +80,10 @@ function add_periplasm_transporters!(model)
 end
 
 function add_membrane_transporters!(model)
-    amino_acids =
-        String.(
-            DataFrame(
-                XLSX.readtable(
-                    joinpath("data", "curation", "curated", "base_reactions.xlsx"),
-                    "aminoacids",
-                ),
-            ).KeGG
-        )
-
+   
     df = DataFrame(
-        XLSX.readtable(
-            joinpath("data", "curation", "curated", "base_reactions.xlsx"),
-            "transporters",
+        CSV.File(
+            joinpath("data", "model", "transporters.csv"),
         ),
     )
     @select!(df, :Type, :Metabolite, :KeggID, :Protein, :Stoichiometry, :Subunit)
