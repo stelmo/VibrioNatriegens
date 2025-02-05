79507769d5af9aa11f80c0611c8db154ef3ddb6a
St. Elmo Wilken <stelmozors@gmail.com>
2025-02-05 16:48:52 +0100

    remove fatty acids, add back manually

--- src/gapfill.jl
+++ src/gapfill.jl
@@ -3,28 +3,29 @@ function gapfill!(model)
 
     rhea_rids = [
         54528 # aldehydo-D-ribose 5-phosphate <-> D-ribose 5-phosphate
-        21387
-        41935
-        50947
-        17972
-        30122
-        43439
-        31254
         19048 # pyruvate to citramalate, c5 branched dibasic metabolism
-        30134
-        30118
-        30126
-        19740
-        32630
-        31462
-        45139
-        22564
         15652 # Spontaneous
         28237 # Spontaneous
         22491 # formaldehyde detox
         21371 # urate to 5-hydroxyisourate
         17032 # s-allantoin to allantoate
+        # 30118
+        # 30122
+        # 30126
+        # 30134
+        # 41935
+        21387
+        50947
+        17972
+        43439
+        31254
+        19740
+        32630
+        31462
+        45139
+        # 42703 look at this one, gene yes, should be gone?
     ]
+
 
     n = length(rhea_rids)
     df = DataFrame(
