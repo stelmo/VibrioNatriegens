ebd44b269347f9b73d903248ea254f2b6c6448e2
St. Elmo Wilken <stelmozors@gmail.com>
2025-02-06 19:06:55 +0100

    add back fatty acids

--- src/gapfill.jl
+++ src/gapfill.jl
@@ -9,20 +9,26 @@ function gapfill!(model)
         22491 # formaldehyde detox
         21371 # urate to 5-hydroxyisourate
         17032 # s-allantoin to allantoate
+        63204 # release acp octadecanoate
+        41932 #release acp hexadecanoate
+        30123 #release acp tetradecanoate
+        30119 #release acp dodecanoate
+        30115 #release acp decanoate
+        30131 #release acp octanoate
         # 30118
         # 30122
         # 30126
         # 30134
         # 41935
-        21387
-        50947
-        17972
-        43439
-        31254
-        19740
-        32630
-        31462
-        45139
+        # 21387
+        # 50947
+        # 17972
+        # 43439
+        # 31254
+        # 19740
+        # 32630
+        # 31462
+        # 45139
         # 42703 look at this one, gene yes, should be gone?
     ]
 
