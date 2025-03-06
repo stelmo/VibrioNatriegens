81fcf036cf396b416e1c47565713f02b7941c0d9
St. Elmo Wilken <stelmozors@gmail.com>
2025-03-06 21:43:46 +0100

    implement all tests

--- test/basics.jl
+++ test/basics.jl
@@ -78,8 +78,48 @@ end
 end
 
 @testset "Deadend metabolites" begin
-    # metabolites that can only be produced or consumed
-    model = VibrioNatriegens.build_model()
+
+    # open all exchanges, transporters, and find metabolites that can only be produced or consumed
+    @everywhere begin
+        model = VibrioNatriegens.build_model()
+        ex_rids = filter(startswith("EX_"), A.reactions(model))
+        transporters =
+            [rid for rid in A.reactions(model) if model.reactions[rid].transporter]
+        for rid in [ex_rids; transporters]
+            model.reactions[rid].lower_bound = -1000
+            model.reactions[rid].upper_bound = 1000
+        end
+        model.reactions["ATPM"].lower_bound = 0.0
+        model.reactions["temp_reaction"] = VibrioNatriegens.Reaction(
+            stoichiometry=Dict(),
+            lower_bound = -1000.0,
+            upper_bound = 1000.0,
+        )
+    end
+
+    mids = filter(!occursin("_"), A.metabolites(model))
+        
+    vs = screen(mids, workers = workers()) do mid
+        model.reactions["temp_reaction"].stoichiometry = Dict(mid => -1.0)
+        ct = flux_balance_constraints(model) 
+    
+        lb = optimized_values(
+            ct,
+            objective = ct.fluxes.temp_reaction.value,
+            sense = Minimal,
+            optimizer = Gurobi.Optimizer,
+        )
+        ub = optimized_values(
+            ct,
+            objective = ct.fluxes.temp_reaction.value,
+            sense = Maximal,
+            optimizer = Gurobi.Optimizer,
+        )
+        (
+            isnothing(lb) ? 0 : abs(lb.fluxes.temp_reaction),
+            isnothing(ub) ? 0 : abs(ub.fluxes.temp_reaction),
+        )
+    end
 
 end
 
